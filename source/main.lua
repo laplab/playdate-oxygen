e8f39c92a5c5e260af5422db6284a808be50707f
Nikita Lapkov <nikita.lapkov@gmail.com>
2022-07-31 11:52:32 +0100

    Load levels from LDtk

--- source/main.lua
+++ source/main.lua
@@ -1,91 +1,77 @@
--- Name this file `main.lua`. Your game can use multiple source files if you wish
--- (use the `import "myFilename"` command), but the simplest games can be written
--- with just `main.lua`.
-
--- You'll want to import these in just about every project you'll work on.
-
 import "CoreLibs/object"
 import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/timer"
 
--- Declaring this "gfx" shorthand will make your life easier. Instead of having
--- to preface all graphics calls with "playdate.graphics", just use "gfx."
--- Performance will be slightly enhanced, too.
--- NOTE: Because it's local, you'll have to do it in every .lua source file.
+import "external/ldtk.lua"
 
 local gfx <const> = playdate.graphics
 
--- Here's our player sprite declaration. We'll scope it to this file because
--- several functions need to access it.
+local game = {}
 
-local playerSprite = nil
+local layerSprites = {}
 
--- A function to set up our game environment.
+function world_shutdown()
+    for layer_name in pairs(LDtk.get_layers(game.level_name)) do
+        layerSprites[layer_name]:remove()
+        layerSprites[layer_name] = nil
+    end
 
-function myGameSetUp()
-
-    -- Set up the player sprite.
-    -- The :setCenter() call specifies that the sprite will be anchored at its center.
-    -- The :moveTo() call moves our sprite to the center of the display.
-
-    local playerImage = gfx.image.new("Images/player")
-    assert( playerImage ) -- make sure the image was where we thought
-
-    playerSprite = gfx.sprite.new( playerImage:scaledImage(3) )
-    playerSprite:moveTo( 200, 120 ) -- this is where the center of the sprite is placed; (200,120) is the center of the Playdate screen
-    playerSprite:add() -- This is critical!
-
-    -- We want an environment displayed behind our sprite.
-    -- There are generally two ways to do this:
-    -- 1) Use setBackgroundDrawingCallback() to draw a background image. (This is what we're doing below.)
-    -- 2) Use a tilemap, assign it to a sprite with sprite:setTilemap(tilemap),
-    --       and call :setZIndex() with some low number so the background stays behind
-    --       your other sprites.
-    gfx.sprite.setBackgroundDrawingCallback(
-        function( x, y, width, height )
-            gfx.setColor(gfx.kColorBlack)
-            gfx.fillRect(x, y, width, height)
-        end
-    )
-
+	LDtk.release_level( game.level_name )
 end
 
--- Now we'll call the function above to configure our game.
--- After this runs (it just runs once), nearly everything will be
--- controlled by the OS calling `playdate.update()` 30 times a second.
+function setup_level(level_name)
+    local previous_level = game.level_name
+	game.level_name = level_name
 
-myGameSetUp()
+	LDtk.load_level(level_name)
+	LDtk.release_level(previous_level)
 
--- `playdate.update()` is the heart of every Playdate game.
--- This function is called right before every frame is drawn onscreen.
--- Use this function to poll input, run game logic, and move sprites.
+	playdate.graphics.sprite.removeAll()
+
+	layerSprites = {}
+	for layer_name, layer in pairs(LDtk.get_layers(level_name)) do
+		if not layer.tiles then
+			goto continue
+		end
+
+		local tilemap = LDtk.create_tilemap(level_name, layer_name)
+
+		local layerSprite = playdate.graphics.sprite.new()
+		layerSprite:setTilemap(tilemap)
+		layerSprite:moveTo(0, 0)
+		layerSprite:setCenter(0, 0)
+		layerSprite:setZIndex(layer.zIndex)
+		layerSprite:add()
+		layerSprites[layer_name] = layerSprite
+
+		local emptyTiles = LDtk.get_empty_tileIDs(level_name, "Solid", layer_name)
+
+		if emptyTiles then
+			playdate.graphics.sprite.addWallSprites(tilemap, emptyTiles)
+		end
+
+		::continue::
+	end
+
+    playdate.graphics.sprite.setAlwaysRedraw(true)
+end
+
+function init()
+    local use_ldtk_precomputed_levels = not playdate.isSimulator
+    LDtk.load("levels/world.ldtk", use_ldtk_precomputed_levels)
+
+    if playdate.isSimulator then
+        LDtk.export_to_lua_files()
+    end
+
+    setup_level("Level_0")
+end
+
+init()
 
 function playdate.update()
-
-    -- Poll the d-pad and move our player accordingly.
-    -- (There are multiple ways to read the d-pad; this is the simplest.)
-    -- Note that it is possible for more than one of these directions
-    -- to be pressed at once, if the user is pressing diagonally.
-
-    if playdate.buttonIsPressed( playdate.kButtonUp ) then
-        playerSprite:moveBy( 0, -2 )
-    end
-    if playdate.buttonIsPressed( playdate.kButtonRight ) then
-        playerSprite:moveBy( 2, 0 )
-    end
-    if playdate.buttonIsPressed( playdate.kButtonDown ) then
-        playerSprite:moveBy( 0, 2 )
-    end
-    if playdate.buttonIsPressed( playdate.kButtonLeft ) then
-        playerSprite:moveBy( -2, 0 )
-    end
-
-    -- Call the functions below in playdate.update() to draw sprites and keep
-    -- timers updated. (We aren't using timers in this example, but in most
-    -- average-complexity games, you will.)
-
+    gfx.setBackgroundColor(gfx.kColorBlack)
     gfx.sprite.update()
     playdate.timer.updateTimers()
-
 end
