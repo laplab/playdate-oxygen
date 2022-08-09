9f323346a68aaecc9f59c10060e6d72824d45d4e
Nikita Lapkov <nikita.lapkov@gmail.com>
2022-08-09 21:14:52 +0100

    Improve camera

--- source/physics/camera.lua
+++ source/physics/camera.lua
@@ -6,6 +6,6 @@ function update_follow_camera(player)
     local screen_width, screen_height = playdate.display.getSize()
     local player_width, player_height = player.sprite:getSize()
     local camera_x = screen_width / 2 - player.sprite.x - player_width / 2
-    local camera_y = screen_height * 3 / 4 - player.sprite.y - player_height / 2
+    local camera_y = screen_height / 2 - player.sprite.y - player_height / 2
     gfx.setDrawOffset(camera_x, camera_y)
 end
