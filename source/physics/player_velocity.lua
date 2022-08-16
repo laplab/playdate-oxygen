c544315c32c65fc9e6b88639ffed93950cb2bc83
Nikita Lapkov <nikita.lapkov@gmail.com>
2022-08-16 19:46:29 +0100

    Fix the logic applying velocity

--- source/physics/player_velocity.lua
+++ source/physics/player_velocity.lua
@@ -14,13 +14,10 @@ function update_player_velocity(player, dt)
     -- Horizontal movement
     if not playdate.buttonIsPressed(playdate.kButtonLeft | playdate.kButtonRight) then
         player.velocity.x = approach(player.velocity.x, 0, PLAYER_GROUND_FRICTION, dt)
-    end
-
-    if not player.attached_left and playdate.buttonIsPressed(playdate.kButtonLeft) then
+    elseif not player.attached_left and playdate.buttonIsPressed(playdate.kButtonLeft) then
         player.velocity.x = -PLAYER_MAX_H_SPEED
         player.flip = true
-    end
-    if not player.attached_right and playdate.buttonIsPressed(playdate.kButtonRight) then
+    elseif not player.attached_right and playdate.buttonIsPressed(playdate.kButtonRight) then
         player.velocity.x = PLAYER_MAX_H_SPEED
         player.flip = false
     end
@@ -35,30 +32,24 @@ function update_player_velocity(player, dt)
             jump_timer:remove()
         end
         jump_timer = playdate.timer.new(500)
-    end
-
-    if jump_timer and jump_timer.timeLeft ~= 0 and not playdate.buttonIsPressed(playdate.kButtonA) then
+    elseif jump_timer and jump_timer.timeLeft ~= 0 and not playdate.buttonIsPressed(playdate.kButtonA) then
         if player.velocity.y < 0 then
             player.velocity.y /= 2
         end
 
         jump_timer:remove()
         jump_timer = nil
-    end
-
-    if (player.attached_left or player.attached_right) and playdate.buttonJustPressed(playdate.kButtonA) then
+    elseif (player.attached_left or player.attached_right) and playdate.buttonJustPressed(playdate.kButtonA) then
         local attach_sign = 1
         if player.attached_right then
             attach_sign = -1
         end
 
         player.velocity.y -= JUMP_SPEED
-        player.velocity.x = 10 * attach_sign * PLAYER_MAX_H_SPEED
-    end
-
-    if not player.grounded then
+        player.velocity.x = 3 * attach_sign * PLAYER_MAX_H_SPEED
+    elseif not player.grounded then
         if player.attached_left or player.attached_right then
-            player.velocity.y = ATTACHED_SPEED
+            player.velocity.y = 0 -- ATTACHED_SPEED
         else
             local gravityMultiplier = 1
             if player.velocity.y > 0 then
