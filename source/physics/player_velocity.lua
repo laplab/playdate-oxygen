86ae3bd98cc3bdff4a1814fe0577f2933493f03d
Nikita Lapkov <nikita.lapkov@gmail.com>
2022-08-16 21:33:26 +0100

    Add turned off wall jump cooldown timer for future experimentation

--- source/physics/player_velocity.lua
+++ source/physics/player_velocity.lua
@@ -4,10 +4,15 @@ import "physics/util"
 import "balance"
 
 local jump_timer = nil
+local wall_jump_cooldown_timer = nil
 
 function compute_horizontal_velocity(player, current_vx, dt)
+    -- Player horizontal movement is disabled for a split second
+    -- after the wall jump to make some distance from the wall.
+    local can_move = not wall_jump_cooldown_timer or wall_jump_cooldown_timer.timeLeft == 0
+
     -- INPUT LEFT
-    if playdate.buttonIsPressed(playdate.kButtonLeft) then
+    if can_move and playdate.buttonIsPressed(playdate.kButtonLeft) then
         player.flip = true
 
         if player.attached_left then
@@ -19,7 +24,7 @@ function compute_horizontal_velocity(player, current_vx, dt)
         end
 
     -- INPUT RIGHT
-    elseif playdate.buttonIsPressed(playdate.kButtonRight) then
+    elseif can_move and playdate.buttonIsPressed(playdate.kButtonRight) then
         player.flip = false
 
         if player.attached_right then
@@ -42,7 +47,7 @@ function compute_vertical_velocity(player, current_vy, dt)
     -- TODO: It is possible that jump will not be registered if the button was pressed
     --       on the first frame, when the 'player.grounded == false'
     if player.grounded and playdate.buttonJustPressed(playdate.kButtonA) then
-        jump_timer = playdate.timer.new(500)
+        jump_timer = playdate.timer.new(GROUND_JUMP_MAX_TIME)
 
         return nil, current_vy - JUMP_SPEED
 
@@ -63,6 +68,8 @@ function compute_vertical_velocity(player, current_vy, dt)
         if player.attached_right then
             attach_sign = -1
         end
+
+        wall_jump_cooldown_timer = playdate.timer.new(WALL_JUMP_COOLDOWN_TIME)
 
         return 3 * attach_sign * PLAYER_MAX_H_SPEED, current_vy - JUMP_SPEED
 
@@ -97,9 +104,16 @@ end
 
 function update_player_velocity(player, dt)
     -- If player touches any surface, current jump is immediately finished.
-    if (player.grounded or player.attached_left or player.attached_right) and jump_timer then
-        jump_timer:remove()
-        jump_timer = nil
+    if player.grounded or player.attached_left or player.attached_right then
+        if jump_timer then
+            jump_timer:remove()
+            jump_timer = nil
+        end
+
+        if wall_jump_cooldown_timer then
+            wall_jump_cooldown_timer:remove()
+            wall_jump_cooldown_timer = nil
+        end
     end
 
     player.velocity.x, player.velocity.y = compute_velocity_vector(player, player.velocity.x, player.velocity.y, dt)
